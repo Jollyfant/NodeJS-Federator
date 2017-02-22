20567faeacd4e1727590fc653c17f6f3e275507a
Jollyfant <koymans@knmi.nl>
2017-02-22 08:26:08 +0000

    Enabled propogation of metric filtering

--- routes/wfcatalog/query/index.js
+++ routes/wfcatalog/query/index.js
@@ -36,17 +36,6 @@ module.exports = function(Federator) {
       return new FederatorError(req, res, ERROR.QUERYSTRING_INVALID); 
     }
 
-    // Check for allowed parameters from static allowed object
-    // and check match the input against a valid regex
-    for(var key in req.query) {
-      if(!ALLOWED.WFCATALOG.hasOwnProperty(key)) {
-        return new FederatorError(req, res, ERROR.INVALID_PARAMETER, key);
-      }
-      if(ALLOWED.WFCATALOG[key] && !REGEX[ALLOWED.WFCATALOG[key]].test(req.query[key])) {
-        return new FederatorError(req, res, ERROR.INVALID_REGEX, key);
-      }
-    }
-
     // GET query parameters
     var stream = {
       "net": req.query.net || req.query.network || null,
@@ -67,52 +56,43 @@ module.exports = function(Federator) {
         "csegments": req.query.csegments || null,
         "sample_rate": req.query.sample_rate || null,
         "record_length": req.query.record_length || null,
-        "num_samples": req.query.num_samples || null,
-        "sample_max": req.query.sample_max || null,
-        "sample_min": req.query.sample_min || null,
-        "sample_rms": req.query.sample_rms || null,
-        "sample_stdev": req.query.sample_stdev || null,
-        "sample_mean": req.query.sample_mean || null,
-        "sample_median": req.query.sample_median || null,
-        "sample_lower_quartile": req.query.sample_lower_quartile || null,
-        "sample_upper_quartile": req.query.sample_upper_quartile || null,
-        "num_gaps": req.query.num_gaps || null,
-        "num_overlaps": req.query.num_overlaps || null,
-        "max_gap": req.query.max_gap || null,
-        "max_overlap": req.query.max_overlap || null,
-        "sum_gaps": req.query.sum_gaps || null,
-        "sum_overlaps": req.query.sum_overlaps || null,
-        "percent_availability": req.query.percent_availability || null,
-        "timing_quality": req.query.timing_quality || null,
-        "timing_quality_median": req.query.timing_quality_median || null,
-        "timing_quality_mean": req.query.timing_quality_mean || null,
-        "timing_quality_upper_quartile": req.query.timing_quality_upper_quartile || null,
-        "timing_quality_lower_quartile": req.query.timing_quality_lower_quartile || null,
-        "timing_quality_min": req.query.timing_quality_min || null,
-        "timing_quality_max": req.query.timing_quality_max || null,
-        "timing_correction": req.query.timing_correction || null,
-        "amplifier_saturation": req.query.amplifier_saturation || null,
-        "digitizer_clipping": req.query.digitizer_clipping || null,
-        "spikes": req.query.spikes || null,
-        "glitches": req.query.glitches || null,
-        "missing_padded_data": req.query.missing_padded_data || null,
-        "telemetry_sync_error": req.query.telemetry_sync_error || null,
-        "digital_filter_charging": req.query.digital_filter_charging || null,
-        "suspect_time_tag": req.query.suspect_time_tag || null,
-        "calibration_signal": req.query.calibration_signal || null,
-        "time_correction_applied": req.query.time_correction_applied || null,
-        "event_begin": req.query.event_begin || null,
-        "event_end": req.query.event_end || null,
-        "positive_leap": req.query.positive_leap || null,
-        "negative_leap": req.query.negative_leap || null,
-        "event_in_progress": req.query.event_in_progress || null,
-        "station_volume": req.query.station_volume || null,
-        "long_record_read": req.query.long_record_read || null,
-        "short_record_read": req.query.short_record_read || null,
-        "start_time_series": req.query.start_time_series || null,
-        "end_time_series": req.query.end_time_series || null,
-        "clock_locked": req.query.clock_locked || null
       }
+    }
+
+    // Check for allowed parameters from static allowed object
+    // and check match the input against a valid regex
+    for(var key in req.query) {
+
+      // Key is metric or has extension
+      if(ALLOWED.WFCATALOG_METRICS.hasOwnProperty(key) || /_eq$|_ne$|_gt$|_ge$|_lt$|_le$/.test(key)) {
+
+        var metric = key.split("_")[0];
+
+        // Confirm key is present
+        if(!ALLOWED.WFCATALOG_METRICS.hasOwnProperty(metric)) {
+          return new FederatorError(req, res, ERROR.INVALID_PARAMETER, key);
+        }
+
+        // Check the type
+        if(!REGEX[ALLOWED.WFCATALOG_METRICS[metric]].test(req.query[key])) {
+          return new FederatorError(req, res, ERROR.INVALID_REGEX, key);
+        }
+
+        // Add to the extension
+        stream.extension[key] = req.query[key];
+
+      } else {
+
+        if(!ALLOWED.WFCATALOG.hasOwnProperty(key)) {
+          return new FederatorError(req, res, ERROR.INVALID_PARAMETER, key);
+        }
+
+        if(ALLOWED.WFCATALOG[key] && !REGEX[ALLOWED.WFCATALOG[key]].test(req.query[key])) {
+          return new FederatorError(req, res, ERROR.INVALID_REGEX, key);
+        }
+
+      }
+
     }
 
     // Confirm start and endtime 
